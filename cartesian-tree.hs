28214ebe1cc16a5b52ea3219c114217e196a8dfc
Brett Wines <brettwines@gmail.com>
2014-05-15 00:09:22 -0700

    Tried parent pointers. About to do zippers instead.

--- cartesian-tree.hs
+++ cartesian-tree.hs
@@ -1,12 +1,18 @@
 
-module CartesianTree
-( CartesianTree(..)
-, CartesianTree.fromList
+module CTree
+( CTree(..)
+, CTree.fromList
 ) where
 
+import qualified Data.Map as M
 import Stack
 import HLib
 
+type CTree a = CartesianTree a
+
+data Direction = L | R deriving (Show, Eq)
+
+-- Node e l r p
 data CartesianTree a
 	= Empty
 	| Node a (CartesianTree a) (CartesianTree a) deriving Show
@@ -20,51 +26,108 @@ instance (Eq a) => Eq (CartesianTree a) where
 		&& (left  x) == (left  y)
 		&& (right x) == (right y)
 
-left  :: CartesianTree a -> CartesianTree a
-right :: CartesianTree a -> CartesianTree a
-left  (Node _ l _) = l
-right (Node _ _ r) = r
+instance (Ord a) => Ord (CartesianTree a) where
+	Empty < _ = True
+	_ < Empty = False
+	x < y = (value x) < (value y)
 
-value :: CartesianTree a -> a
+left   :: CTree a -> CTree a
+right  :: CTree a -> CTree a
+left   (Node _ l _) = l
+right  (Node _ _ r) = r
+
+value :: CTree a -> a
 value Empty = error "Can't get the value out of an empty node."
-value (Node a _ _) = a
+value (Node e _ _) = e
 
--- http://www.stanford.edu/class/cs166/lectures/01/Slides01.pdf
-fromList :: (Ord a) => [a] -> CartesianTree a
-fromList list = fromList' list Empty Stack.empty
+fromList :: (Ord a) => [a] -> CTree a
+fromList list = fromList' list Empty Empty
 
-fromList' :: (Ord a) => 
-	[a]
-	-> CartesianTree a
-	-> Stack (CartesianTree a)
-	-> CartesianTree a 
-fromList' [] tree _ = tree
-fromList' list tree stack = fromList' list' tree' stack'
+-- returns the new root of the tree
+update_node :: (Ord a) => CTree a -> CTree a -> (CTree a, CTree a)
+update_node old new = (new, new_root)
+	where new_root = update_node' old new
+
+update_node' :: (Ord a) => CTree a -> CTree a -> CTree a
+update_node' old new
+	| is_root old = new
+	| otherwise =
+		let
+			old_parent@(Node e l r p dir) = parent old
+			new_parent = if dir == L
+				then Node e new r   p dir
+				else Node e l   new p dir
+		in
+			update_node' old_parent new_parent
+
+type CTMap a = M.Map (CTree a) (CTree a)
+
+old_to_new_mapping :: (Ord a) => CTree a -> CTree a -> CTMap a
+old_to_new_mapping old new = old_to_new_mapping' old new M.empty
+
+old_to_new_mapping' :: (Ord a) => CTree a -> CTree a -> CTMap a -> CTMap a
+old_to_new_mapping' old new m =
+	let
+		m' = M.insert old new m
+	in
+		if (is_root old)
+			then m'
+			else old_to_new_mapping' (parent old) (parent new) m'
+
+-- circular dependency means never set properly?
+set_parent :: CTree a -> CTree a -> (CTree a, CTree a)
+set_parent n Empty = (n, n)
+set_parent
+	node_with_unset_parent@(Node e  l  r  p  dir)
+	future_parent_of_node@(Node  e' l' r' p' dir')
+	= (node_with_set_parent, new_root)
+		where
+			n = node_with_unset_parent
+			parent_with_child_set = if dir == L
+				then Node e' n  r' p' dir'
+				else Node e' l' n  p' dir'
+			(new_parent, new_root) = set_parent parent_with_child_set p'
+
+			node_with_set_parent = Node e l r new_parent dir
+
+-- elem of parent, other child of parent
+data Focus a = Left a (CartesianTree a) | Right a (CartesianTree a) deriving Show
+type Foci = [Focus]
+type Zipper a = (Foci a, CartesianTree a)
+
+fromList' :: (Ord a) => [a] -> Zipper a -> CTree a 
+fromList' [] zipper@(foci, tree) _ = tree
+fromList' list zipper@(foci, tree) = fromList' list' zipper'
 	where
 		e = head list
 		list' = tail list
 
-		(stack_front, stack_back) = takeWhileAndRest f stack
-			where f _ = (not . is_empty $ stack) &&
-						((value . peek $ stack) >= e)
+		(rspine_gt, rspine_lt) = takeWhileAndRest f $ rspine
+			where
+				rspine = takeWhile (not . is_root) . iterate parent $ rmost_node
+				f node = (not . is_empty $ rspine) &&
+						((value node) >= e)
 
-		last_popped_elem = last_maybe stack_front
+		last_popped_elem = last_maybe rspine_gt
 			where last_maybe l = if l /= [] then Just (last l) else Nothing
 		
-		new_node = if last_popped_elem /= Nothing
-			then Node e (from_just last_popped_elem) Empty
-			else Node e Empty Empty
+		new_node_with_unset_parent = if last_popped_elem /= Nothing
+			then Node e (from_just last_popped_elem) Empty Empty R
+			else Node e Empty Empty Empty R
 
-		tree' = if stack_back == []
-			then new_node
-			else
-				let
-					root@(Node elem l r) = peek stack_back
-				in
-					Node elem l new_node -- r will be Empty all the time
+		(new_root, tree') = if rspine_lt == []
+			then (new_node_with_unset_parent, new_node_with_unset_parent)
+			else (new_node, new_root)
+				where
+					future_parent_of_new_node = peek rspine_lt
+					(new_node, new_root) = set_parent
+						new_node_with_unset_parent
+						future_parent_of_new_node
 
-		stack' = push new_node $
-			if stack_back /= [] -- i.e. if we didn't just place a new root, but rather "modified" the old one
-				then push tree' . pop $ stack_back
-				else stack_back
+		zipper' =
+
+
+
+
+
 
