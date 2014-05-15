8f32b28d9fbc89054f65d760d6cf6c8d7eca86e8
Brett Wines <brettwines@gmail.com>
2014-05-15 02:24:10 -0700

    Implemented Cartesian tree with zippers.

--- cartesian-tree.hs
+++ cartesian-tree.hs
@@ -10,9 +10,6 @@ import HLib
 
 type CTree a = CartesianTree a
 
-data Direction = L | R deriving (Show, Eq)
-
--- Node e l r p
 data CartesianTree a
 	= Empty
 	| Node a (CartesianTree a) (CartesianTree a) deriving Show
@@ -23,109 +20,133 @@ instance (Eq a) => Eq (CartesianTree a) where
 	_ == Empty = False
 	x == y =
 		(value x) == (value y)
-		&& (left  x) == (left  y)
-		&& (right x) == (right y)
+		&& (left_child  x) == (left_child  y)
+		&& (right_child x) == (right_child y)
 
 instance (Ord a) => Ord (CartesianTree a) where
 	Empty < _ = True
 	_ < Empty = False
 	x < y = (value x) < (value y)
 
-left   :: CTree a -> CTree a
-right  :: CTree a -> CTree a
-left   (Node _ l _) = l
-right  (Node _ _ r) = r
+left_child :: CTree a -> CTree a
+left_child (Node _ l _) = l
+
+right_child :: CTree a -> CTree a
+right_child (Node _ _ r) = r
 
 value :: CTree a -> a
 value Empty = error "Can't get the value out of an empty node."
 value (Node e _ _) = e
 
-fromList :: (Ord a) => [a] -> CTree a
-fromList list = fromList' list Empty Empty
-
--- returns the new root of the tree
-update_node :: (Ord a) => CTree a -> CTree a -> (CTree a, CTree a)
-update_node old new = (new, new_root)
-	where new_root = update_node' old new
-
-update_node' :: (Ord a) => CTree a -> CTree a -> CTree a
-update_node' old new
-	| is_root old = new
-	| otherwise =
-		let
-			old_parent@(Node e l r p dir) = parent old
-			new_parent = if dir == L
-				then Node e new r   p dir
-				else Node e l   new p dir
-		in
-			update_node' old_parent new_parent
-
-type CTMap a = M.Map (CTree a) (CTree a)
-
-old_to_new_mapping :: (Ord a) => CTree a -> CTree a -> CTMap a
-old_to_new_mapping old new = old_to_new_mapping' old new M.empty
-
-old_to_new_mapping' :: (Ord a) => CTree a -> CTree a -> CTMap a -> CTMap a
-old_to_new_mapping' old new m =
-	let
-		m' = M.insert old new m
-	in
-		if (is_root old)
-			then m'
-			else old_to_new_mapping' (parent old) (parent new) m'
-
--- circular dependency means never set properly?
-set_parent :: CTree a -> CTree a -> (CTree a, CTree a)
-set_parent n Empty = (n, n)
-set_parent
-	node_with_unset_parent@(Node e  l  r  p  dir)
-	future_parent_of_node@(Node  e' l' r' p' dir')
-	= (node_with_set_parent, new_root)
-		where
-			n = node_with_unset_parent
-			parent_with_child_set = if dir == L
-				then Node e' n  r' p' dir'
-				else Node e' l' n  p' dir'
-			(new_parent, new_root) = set_parent parent_with_child_set p'
-
-			node_with_set_parent = Node e l r new_parent dir
+---------------------------
+--         Zipper        --
+---------------------------
 
 -- elem of parent, other child of parent
-data Focus a = Left a (CartesianTree a) | Right a (CartesianTree a) deriving Show
-type Foci = [Focus]
+data Focus a = L a (CartesianTree a) | R a (CartesianTree a) deriving Show
+type Foci a = [Focus a]
 type Zipper a = (Foci a, CartesianTree a)
 
-fromList' :: (Ord a) => [a] -> Zipper a -> CTree a 
-fromList' [] zipper@(foci, tree) _ = tree
-fromList' list zipper@(foci, tree) = fromList' list' zipper'
-	where
-		e = head list
-		list' = tail list
+go_to_top :: Zipper a -> Zipper a
+go_to_top z = if has_parent z
+	then go_to_top . go_up $ z
+	else z
 
-		(rspine_gt, rspine_lt) = takeWhileAndRest f $ rspine
+go_up :: Zipper a -> Zipper a
+go_up z@([], tree) = error "Already at root"
+go_up z@((L e r):foci, tree) = (foci, Node e tree r)
+go_up z@((R e l):foci, tree) = (foci, Node e l tree)
+
+go_left :: Zipper a -> Zipper a
+go_left z@(foci, Empty) = error "Node is empty"
+go_left z@(foci, Node e l r) = (focus:foci, l)
+	where focus = L e r
+
+go_right :: Zipper a -> Zipper a
+go_right z@(foci, Empty) = error "Node is empty"
+go_right z@(foci, Node e l r) = (focus:foci, r)
+	where focus = R e l
+
+has_parent :: Zipper a -> Bool
+has_parent z@([], _) = False
+has_parent z@(_, _) = True
+
+has_left_child :: Zipper a -> Bool
+has_left_child z@(_, Empty) = False
+has_left_child z@(_, Node _ Empty _) = False
+has_left_child _ = True
+
+has_right_child :: Zipper a -> Bool
+has_right_child z@(_, Empty) = False
+has_right_child z@(_, Node _ _ Empty) = False
+has_right_child _ = True
+
+-- Cartesian tree insertion mechanism:
+-- 
+-- 
+-- 
+-- 
+-- 
+insert_at_point :: (Zipper a, Bool) -> a -> Zipper a
+insert_at_point
+	(z@(foci, root), False)
+	new_elem
+	= ([], new_root) -- foci is [] anyway
+		where new_root = Node new_elem root Empty
+
+insert_at_point
+	(z@(foci, parent@(Node e l r)), True) -- can assume `parent` will never be `Empty` (see `parent_of_new_elem`)
+	new_elem
+	= go_right $ (foci, parent')
+		where
+			new_node_left_child = if has_right_child z
+				then r
+				else Empty
+			
+			parent' = Node e l (Node new_elem new_node_left_child Empty)
+
+---------------------------
+--       fromList        --
+---------------------------
+
+-- False signifies no parent, so we return `Left <root>`
+-- True signifies yes parent (regular case)
+parent_of_new_elem :: (Ord a) => a -> Zipper a -> (Zipper a, Bool)
+parent_of_new_elem new_elem z@(foci, tree@(Node e l r)) =
+	if e <= new_elem
+		then (z, True)
+		else if not . has_parent $ z
+			then (z, False)
+			else parent_of_new_elem new_elem (go_up z)
+{-
+(rspine_gt, rspine_lt) = takeWhileAndRest f $ rspine
 			where
 				rspine = takeWhile (not . is_root) . iterate parent $ rmost_node
 				f node = (not . is_empty $ rspine) &&
 						((value node) >= e)
+-}
 
-		last_popped_elem = last_maybe rspine_gt
-			where last_maybe l = if l /= [] then Just (last l) else Nothing
-		
-		new_node_with_unset_parent = if last_popped_elem /= Nothing
-			then Node e (from_just last_popped_elem) Empty Empty R
-			else Node e Empty Empty Empty R
+-- can make this just a wrapper, probably
+fromList :: (Ord a) => [a] -> CTree a
+fromList [] = Empty
+fromList list =
+	let
+		(e, list') = (head list, tail list)
+		node = Node e Empty Empty
+	in
+		fromList' list' ([], node)
 
-		(new_root, tree') = if rspine_lt == []
-			then (new_node_with_unset_parent, new_node_with_unset_parent)
-			else (new_node, new_root)
-				where
-					future_parent_of_new_node = peek rspine_lt
-					(new_node, new_root) = set_parent
-						new_node_with_unset_parent
-						future_parent_of_new_node
+fromList' :: (Ord a) => [a] -> Zipper a -> CTree a 
+fromList' [] z = snd . go_to_top $ z
+fromList' list z_rspine_last@(foci, tree) = fromList' list' zipper'
+	where
+		e = head list
+		list' = tail list
 
-		zipper' =
+		z_parent_of_new_elem = parent_of_new_elem e z_rspine_last
 
+		-- is this the right location for the zipper? I think so, but...
+		zipper' = insert_at_point z_parent_of_new_elem e
 
 
 
