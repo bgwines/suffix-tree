36a9d7990e5dde10ef1a1e48275556fdfd6df7de
Brett Wines <brettwines@gmail.com>
2014-05-20 01:06:35 -0700

    Construction of preliminary stree.

--- stree.hs
+++ stree.hs
@@ -27,10 +27,9 @@ RMQ
 Algorithm
 ---------
 Build a suffix tree in O(n) (all steps are O(n)):
-	1. Build suffix array `S` // O(n)
+	1. Build suffix array `S`
 	2. Build LCP array `L` of adjacent elems in `S`
-	3. Build RMQ structure over `L`
-	4. Build suffix tree from suffix array and LCP array
+	3. Build suffix tree from suffix array and LCP array
 
 1. Build suffix array (DC3)
 	(i) Recursively get the sorted order of all suffixes starting at positions that aren't multiples of three.
@@ -58,14 +57,12 @@ Build a suffix tree in O(n) (all steps are O(n)):
 
 	(iii) Using a standard merge algorithm (à la mergesort), merge the sorted lists of suffixes together into the overall suffix array.
 		// if two compared letters at indices are same, compare letters after them in string (slide 206)
-	
+
 2. Build LCP array `L` of adjacent elems in `S`
 	// http://www.cs.iastate.edu/~cs548/references/linear_lcp.pdf
 
-3. Build RMQ structure over `L` (Fischer-Heun)
-
-4. Build suffix tree from suffix array and LCP array
-	(i) Construct a Cartesian tree from the LCP array, fusing together nodes with the same values if one becomes a parent of the `other.
+3. Build suffix tree from suffix array and LCP array
+	(i) Construct a Cartesian tree from the LCP array, fusing together nodes with the same values if one becomes a parent of the other.
 		http://www.stanford.edu/class/cs166/lectures/01/Slides01.pdf
 			#112
 
@@ -91,6 +88,18 @@ data STree
 	= Leaf Int
 	| Internal (M.Map String STree) deriving Show
 
+data PreliminarySTree
+	= PLeaf String Int
+	| PInternal String Int [PreliminarySTree] deriving Show
+
+update_string :: (String -> String) -> PreliminarySTree -> PreliminarySTree
+update_string f node@(PLeaf s i) = PLeaf (f s) i
+update_string f node@(PInternal s i children) = PInternal (f s) i children
+
+get_string :: PreliminarySTree -> String
+get_string node@(PLeaf s _) = s
+get_string node@(PInternal s _ _) = s
+
 index :: STree -> Int
 index (Leaf i) = i
 index (Internal _) = error "Internal nodes don't represent ends of suffixes"
@@ -101,8 +110,7 @@ is_leaf (Internal _) = False
 
 -- 1. Build suffix array `S`
 -- 2. Build LCP array `L` of adjacent elems in `S`
--- 3. Build RMQ structure over `L`
--- 4. Build suffix tree from suffix array and LCP array
+-- 3. Build suffix tree from suffix array and LCP array
 
 ---------------------------------------------------------
 --                   Test Structures                   --
@@ -133,8 +141,24 @@ construct_stree str = suffix_array_to_stree str suffix_array fused_ctree
 		fused_ctree :: FusedCTree.FusedCTree Int
 		fused_ctree = FusedCTree.fuse . CTree.fromList . A.elems $ lcps
 
+{-
+let lcps = get_pairwise_adjacent_lcps str test_suffix_array
+let fused_ctree = FusedCTree.fuse . CTree.fromList . A.elems $ lcps
+let prelim_prelim_stree = fst $ fctree_to_prelim_prelim_stree' str test_suffix_array fused_ctree 0
+let prelim_stree = fill_internal_nodes prelim_prelim_stree
+-}
+
 suffix_array_to_stree :: String -> SuffixArray -> FusedCTree.FusedCTree Int -> STree
-suffix_array_to_stree str suffix_array tree = fst $ fctree_to_stree' str suffix_array tree 0	
+suffix_array_to_stree str suffix_array tree = stree
+	where
+		prelim_prelim_stree :: PreliminarySTree
+		prelim_prelim_stree = fst $ fctree_to_prelim_prelim_stree' str suffix_array tree 0	
+
+		prelim_stree :: PreliminarySTree
+		prelim_stree = fill_internal_nodes prelim_prelim_stree
+
+		stree :: STree
+		stree = prelim_stree_to_stree prelim_stree
 
 {-
 data STree
@@ -152,7 +176,7 @@ suffix_array:
 	["$" (8), "e$" (7), "ense$" (4), "nonsense$" (0), "nse$" (5),
 
           5                   6            7          8
-		"nsense$" (2), "onsensee$" (1), "se$" (6), "sense$" (3)]
+		"nsense$" (2), "onsense$" (1), "se$" (6), "sense$" (3)]
 
 fused_ctree:
                                
@@ -175,27 +199,93 @@ fused_ctree:
                 |      /   [ se  0 (*) ]     |           |
           [3 (nse$)  6 ($) ] / \          [ 4 (nse$)   7 ($) ]
                             /   \
-                    [ 2 (nse$)   5 ($) ]
+                      [5 (nse$)  5 ($) ]
 
 	*:"onsense$"
 
+-- preliminary stree, without internal nodes filled:
+
+	PInternal "" 0
+		[PLeaf "$" 8,
+		 PInternal "" 1
+		 	[PLeaf "e$" 7,
+		 	 PLeaf "ense$" 4],
+		 PInternal "" 1
+		 	[PLeaf "nonsense$" 0,
+		 	 PInternal "" 3
+		 	 	[PLeaf "nse$" 5,
+		 	 	 PLeaf "nsense$" 2]
+		 	],
+		 	PLeaf "onsense$" 1,
+		 	PInternal "" 2
+		 		[PLeaf "se$" 6,
+		 		 PLeaf "sense$" 3]
+		]
+
+-- preliminary stree:
+
+	PInternal "" 0
+		[PLeaf "$" 8,
+		 
+		 PInternal "e" 1
+		 	[PLeaf "$" 7,
+		 	 PLeaf "nse$" 4],
+		 
+		 PInternal "n" 1
+		 	[PLeaf "onsense$" 0,
+		 	 PInternal "se" 3
+		 	 	[PLeaf "$" 5,PLeaf "nse$" 2]],
+
+		 PLeaf "onsense$" 1,
+
+		 PInternal "se" 2
+		 	[PLeaf "$" 6,
+		 	 PLeaf "nse$" 3]]
 -}
 
-fctree_to_stree' :: String -> SuffixArray -> FusedCTree.FusedCTree Int -> Int -> (STree, Int)
-fctree_to_stree' str suffix_array fused_ctree i
-	| (FusedCTree.is_empty fused_ctree) = (Leaf (suffix_array A.! i), i+1)
-	| otherwise = (Internal children, i')
-		where
-			children_with_updated_is :: [(STree, Int)]
-			children_with_updated_is = scanl f (undefined, i) $ FusedCTree.get_children fused_ctree
-				where
-					f :: (STree, Int) -> FusedCTree.FusedCTree Int -> (STree, Int)
-					f (_, i'') child = fctree_to_stree' str suffix_array child i''
+fill_internal_nodes :: PreliminarySTree -> PreliminarySTree
+fill_internal_nodes node@(PLeaf _ _) = node
+fill_internal_nodes node@(PInternal s i children) = PInternal s' i corrected_children
+	where
+		filled_children :: [PreliminarySTree]
+		filled_children = map fill_internal_nodes children
 
-			i' :: Int
-			i' = snd . last $ children_with_updated_is
+		corrected_children :: [PreliminarySTree]
+		corrected_children = map f filled_children
 
-			children :: M.Map String STree
+		f :: PreliminarySTree -> PreliminarySTree
+		f node = update_string (drop i) node -- not O(1)!
+
+		s' :: String
+		s' = take i . get_string . head $ children
+
+fctree_to_prelim_prelim_stree' :: String -> SuffixArray -> FusedCTree.FusedCTree Int -> Int -> (PreliminarySTree, Int)
+fctree_to_prelim_prelim_stree' str suffix_array fused_ctree i =
+	if (FusedCTree.is_empty fused_ctree)
+		then
+			let index = suffix_array A.! i
+			in (PLeaf (drop index str) (index), i+1) -- not O(1)!
+		else (PInternal "" lcp children, i') -- "" for now; this is preliminary-preliminary
+			where
+				lcp :: Int
+				lcp = FusedCTree.value fused_ctree
+
+				children_with_updated_is :: [(PreliminarySTree, Int)]
+				children_with_updated_is = tail $ scanl f (undefined, i) $ FusedCTree.get_children fused_ctree
+					where
+						f :: (PreliminarySTree, Int) -> FusedCTree.FusedCTree Int -> (PreliminarySTree, Int)
+						f (_, i'') child = fctree_to_prelim_prelim_stree' str suffix_array child i''
+
+				i' :: Int
+				i' = snd . last $ children_with_updated_is
+
+				children :: [PreliminarySTree]
+				children = map fst children_with_updated_is
+
+
+
+
+{-			children :: M.Map String STree
 			children = foldl insert M.empty . map fst $children_with_updated_is
 				where
 					insert :: M.Map String STree -> STree -> M.Map String STree
@@ -210,7 +300,7 @@ fctree_to_stree' str suffix_array fused_ctree i
 									"" -- for now; we'll do a second pass later that fixes this
 
 							n :: Int
-							n = (length str) - (index node)
+							n = (length str) - (index node)-}
 
 
 
